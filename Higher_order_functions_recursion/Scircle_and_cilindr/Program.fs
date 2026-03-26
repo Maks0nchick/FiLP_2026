b29fc99d32eaffe26f6733a2c96d701e82f4a81d
Maksim <maksimkrapotkin@gmail.com>
2026-03-26 05:13:34 +0300

    |Lab_1|Zad_3|Write a function for calculating the area of a circle|

---
+++ Higher_order_functions_recursion/Scircle_and_cilindr/Program.fs
@@ -0,0 +1,27 @@
+﻿open System
+
+let PI = 3.14
+
+// Функция площади круга
+let SCircle R = PI * R * R
+
+// 1. Реализация через суперпозицию
+let SCilindr_super R length = length * SCircle R
+
+// 2. Реализация через каррирование
+let SCilindr_curr R = fun length -> length * SCircle R
+
+// Ввод данных
+printf "Введите радиус круга: "
+let radius = Double.Parse(Console.ReadLine())
+
+printf "Введите высоту цилиндра: "
+let height = Double.Parse(Console.ReadLine())
+
+// Вычисления
+let resultSuper = SCilindr_super radius height
+let resultCurr = SCilindr_curr radius height
+
+// Вывод результатов
+printfn "Объём цилиндра (суперпозиция): %.2f" resultSuper
+printfn "Объём цилиндра (каррирование): %.2f" resultCurr
