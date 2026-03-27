4577af50682fa0e056277a21ac602c5f10036c21
Maksim <maksimkrapotkin@gmail.com>
2026-03-27 06:06:34 +0300

    |Lab_2|Zad_2|a function that displays a list on the screen|

---
+++ Lists/List_output/Program.fs
@@ -0,0 +1,27 @@
+﻿open System
+
+let rec readList n = 
+    if n = 0 then []
+    else
+        let head = Convert.ToInt32(Console.ReadLine())
+        let tail = readList (n - 1)
+        head :: tail
+
+let writeList list =
+    let rec loop lst =
+        match lst with
+        | [] -> ()
+        | h :: t ->
+            printfn "%d" h
+            loop t
+    loop list
+
+[<EntryPoint>]
+let main argv =
+    printf "Введите количество элементов: "
+    let n = Convert.ToInt32(Console.ReadLine())
+    printfn "Введите %d элементов:" n
+    let lst = readList n
+    printfn "\nСписок:"
+    writeList lst
+    0
