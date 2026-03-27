ff61b18c685d14330b1dd6b720eec6d4de0b9598
Maksim <maksimkrapotkin@gmail.com>
2026-03-27 14:17:57 +0300

    |Lab_2|Zad_8|a function that, for a given list, indicates how many elements from it can be squared|

---
+++ Lists/Number_of_square_elements/Program.fs
@@ -0,0 +1,32 @@
+﻿open System
+
+let rec readList n = 
+    if n = 0 then []
+    else
+        let head = Convert.ToInt32(Console.ReadLine())
+        let tail = readList (n - 1)
+        head :: tail
+
+let writeList list =
+    List.iter (printfn "%d") list
+
+let countSquareElements list =
+    let squares = list |> List.map (fun x -> x * x) |> Set.ofList
+    list 
+    |> List.filter (fun x -> Set.contains x squares)
+    |> List.length
+
+[<EntryPoint>]
+let main argv =
+    printf "Введите количество элементов: "
+    let n = Convert.ToInt32(Console.ReadLine())
+    printfn "Введите %d элементов:" n
+    let lst = readList n
+    
+    printfn "\nИсходный список:"
+    writeList lst
+    
+    let result = countSquareElements lst
+    printfn "\nКоличество элементов, являющихся квадратом другого элемента: %d" result
+    
+    0
