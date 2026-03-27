847d768d14c46f079c64408b6c8c2a908fdd6895
Maksim <maksimkrapotkin@gmail.com>
2026-03-27 05:04:49 +0300

    |Lab_2|Zad_1|Write a function that returns a list read from the keyboard based on a given number n|

---
+++ Lists/Returns_a_list/Program.fs
@@ -0,0 +1,26 @@
+﻿open System
+
+let rec readList n = 
+    if n = 0 then []
+    else
+        let head = Convert.ToInt32(Console.ReadLine())
+        let tail = readList (n - 1)
+        head :: tail
+
+let readChurchList n =
+    printfn "Введите %d элементов:" n
+    readList n
+
+
+[<EntryPoint>]
+let main argv =
+    printfn "Демонстрация работы функции readChurchList"
+    printfn ""
+    
+    printf "Введите количество элементов: "
+    let n = Convert.ToInt32(Console.ReadLine())
+    
+    let result = readChurchList n
+    printfn "\nСозданный список Чёрча: %A" result
+    
+    0
